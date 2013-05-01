ab833e385e0c721a5c0f9de7c9da34bca4bfb64a
John <john_elf@126.com>
2013-05-01 19:58:46 +0800

    add DBConnection test(need to ignore??)

--- core/src/main/java/com/thoughtworks/Configuration.java
+++ core/src/main/java/com/thoughtworks/Configuration.java
@@ -8,13 +8,38 @@ public class Configuration {
     private Properties properties = new Properties();
 
     public Configuration() {
-        InputStream in = getClass().getResourceAsStream("express-orm.properties");
+        InputStream in = getClass().getResourceAsStream("/express-orm.properties");
         if (in != null) {
             try {
                 properties.load(in);
             } catch (Exception e) {
-                //throw new InitException(e);
+                throw new InitException();
             }
         }
+    }
+
+    public String getDriverName() {
+        String driver = properties.getProperty("jdbc.driver");
+        return driver != null ? driver : null;
+    }
+
+    public String getURL() {
+        String url = properties.getProperty("jdbc.url");
+        return url != null ? url : null;
+    }
+
+    public String getUser() {
+        String user = properties.getProperty("jdbc.user");
+        return user != null ? user : null;
+    }
+
+    public String getPassword() {
+        String password = properties.getProperty("jdbc.password");
+        return password != null ? password : null;
+    }
+
+    public String getDB() {
+        String dbName = properties.getProperty("db");
+        return dbName != null ? dbName : null;
     }
 }
--- core/src/main/java/com/thoughtworks/DB.java
+++ core/src/main/java/com/thoughtworks/DB.java
@@ -1,5 +1,7 @@
 package com.thoughtworks;
 
+import com.thoughtworks.exceptions.DBInitException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -11,6 +13,33 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class DB {
     private static ThreadLocal<Connection> connection = new ThreadLocal<Connection>();
+    private String dbName;
+
+    public DB(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public DB() {
+        this.dbName = "test";
+    }
+
+    public void open(String driver, String url, String user, String password) {
+        checkExistingConnection(dbName);
+        try {
+            Class.forName(driver);
+            Connection connection = DriverManager.getConnection(url, user, password);
+            ConnectionManager.attach(dbName, connection);
+        } catch (Exception e) {
+            throw new DBInitException();
+        }
+    }
+
+    private void checkExistingConnection(String dbName) {
+        if (ConnectionManager.getConnections().containsKey(dbName)) {
+            throw new DBInitException("Cannot open a new connection because existing connection is still on current thread, dbName: " + dbName + ", connection instance: " + ConnectionManager.getConnections().get(dbName)
+                    + ". This might indicate a logical error in your application.");
+        }
+    }
 
     public static Connection getConnection() throws IOException, ClassNotFoundException, SQLException {
         if (connection.get() != null) {
